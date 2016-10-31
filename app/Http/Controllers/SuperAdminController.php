3715eaddcdd9a5ed23ed803ff5817efe501802ba
irfankhoirul <irfankhoirul@gmail.com>
2016-10-31 22:39:10 +0700

    login Password checking

--- app/Http/Controllers/SuperAdminController.php
+++ app/Http/Controllers/SuperAdminController.php
@@ -5,15 +5,8 @@ namespace TATravel\Http\Controllers;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Http\Request;
 use TATravel\Http\Requests;
+use TATravel\SuperAdmin;
 
 class SuperAdminController extends Controller {
-
-    //
-
-    public function login(Request $request) {
-        print_r($request -> request -> all());
-//        $token = Input::post('token');
-//        echo "Your Token: " . $token;
-    }
 
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,26 @@
 
 namespace TATravel\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
-
 use TATravel\Http\Requests;
+use TATravel\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //
+    public function login(Request $request) {
+        $user = DB::table('user')->where('email', $request->request->get('email'))->first();
+        $toBeHashed = $request->request->get('password').$user->salt;
+
+        // Check username + password        
+        if(strtoupper(hash('sha512', $toBeHashed)) == $user->password){
+            echo "OKKE";
+        } else {
+            echo "Not Okke";
+        }
+        
+        // Generate token
+        
+    }
 }
