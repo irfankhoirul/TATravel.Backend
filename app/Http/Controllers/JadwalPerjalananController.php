72d514ccb0acf5bcea88322336ab7faafd059b9f
irfankhoirul <irfankhoirul@gmail.com>
2016-12-04 22:18:06 +0700

    add api search and location

--- app/Http/Controllers/JadwalPerjalananController.php
+++ app/Http/Controllers/JadwalPerjalananController.php
@@ -19,7 +19,8 @@ class JadwalPerjalananController extends Controller
      * */
     public function availableSchedule(Request $request)
     {
-        $date = '2016-12-02 09:00:00';
+        $date = $request->request->get('date');
+//        $date = '2016-12-02 09:00:00';
         $schedules = DB::table('jadwal_perjalanan')->whereDate('waktu_keberangkatan', '=', $date)->get()->toArray();
         if (count($schedules) > 0) {
             $newSchedule = array();
@@ -29,20 +30,6 @@ class JadwalPerjalananController extends Controller
                     array_push($newSchedule, $schedule);
                 }
             }
-
-//            $newSchedule = array();
-//            foreach ($schedules as $schedule){
-//                $admins = array();
-//                if($schedule['id_admin'] != NULL){
-//                    $scdl = DB::table('admin')->where('id', $schedule['id_admin'])->first();
-//                    array_push($admins, $scdl);
-//                    array_push($admins, $scdl);
-//                    array_push($admins, $scdl);
-//                }
-//                $schedule['admins'] = $admins;
-//                array_push($newSchedule, $schedule);
-//            }
-
             self::renderResult(1, "Berhasil", $newSchedule, TRUE);
         } else {
             self::renderResult(0, "Tidak ada data", null, null);
--- app/Http/Controllers/LokasiController.php
+++ app/Http/Controllers/LokasiController.php
@@ -3,16 +3,29 @@
 namespace TATravel\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use TATravel\Http\Requests;
 
-class LokasiController extends Controller
-{
+use Illuminate\Support\Facades\DB;
+
+class LokasiController extends Controller {
+
     /**
      * Memberikan list lokasi pemberangkatan dan tujuan dari operator travel
      * */
-    public function getListLocation()
-    {
-
+    public function availableLocation() {
+        $locations = DB::table('lokasi')->get()->toArray();
+        if(count($locations) > 0){
+            $newLocations = array();
+            foreach ($locations as $location){
+                if ($location['id_operator_travel'] != NULL) {
+                    $location['operatorTravel'] = DB::table('operator_travel')->where('id', $location['id_operator_travel'])->first();
+                    array_push($newLocations, $location);
+                }
+            }
+            self::renderResult(1, "Berhasil", $newLocations, TRUE);
+        } else {
+            self::renderResult(0, "Tidak Ada Data", NULL, NULL);
+        }
     }
+
 }
